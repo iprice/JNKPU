799a72cb6037002f220493ae088b997c26c8ea63
Iain Price (LUNA) <git@predestined.net>
2019-03-27 19:33:53 +0000

    Support command line options, support ipv6, support strict/relaxed error handling, change version number

--- src/net/coagulate/JNKPU/NetworkUnlock.java
+++ src/net/coagulate/JNKPU/NetworkUnlock.java
@@ -1,16 +1,6 @@
 package net.coagulate.JNKPU;
 
-import java.net.*;
-import java.security.*;
-import java.security.spec.*;
-import java.io.*;
-import javax.crypto.*;
-import javax.crypto.spec.*;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.crypto.macs.*;
-import org.bouncycastle.crypto.modes.*;
-import org.bouncycastle.crypto.params.*;
-import org.bouncycastle.crypto.engines.*;
+import java.io.IOException;
 
 
 
@@ -24,20 +14,68 @@ import org.bouncycastle.crypto.engines.*;
 public class NetworkUnlock {
 
 	public static final boolean DEBUG=false;
-        public static final String VERSION="1.0.0";
-        public static final String RELEASE="20170830";
+        public static final String VERSION="1.1.0";
+        public static final String RELEASE="20190327";
+        
+        private static boolean startipv4=true;
+        private static boolean startipv6=true;
+        private static boolean strict=true;
+        private static String keyfile=null;
 
 	public static void main(String args[]) {
 
             System.out.println("Java Network Key Protector Unlocker version "+VERSION+" ("+RELEASE+")");
-
-            // One day a better command line parser might help.
-            if (args.length!=1) { System.err.println("JNKPU takes exactly one command line argument, the path to the private key (PKCS8, no password)"); System.exit(1); }
+            //-----
+            parseArguments(args);
+            //-----
             // Load the private key and run pre-flight checks on the cryptography providers
-            Cryptography.init(args[0]);
-            
-            // eventually we would start both DHCP4 and DHCP6 listeners here, but one thing at a time
-            DHCPv4 listenerv4=new DHCPv4();
-            listenerv4.start();
+            if (keyfile==null || keyfile.isEmpty()) { System.err.print("You must specify a key file"); usage(); System.exit(1); }
+            System.out.println("Loading private key...");
+            Cryptography.init(keyfile);
+            //-----
+            if (startipv4) {
+                try {
+                    System.out.println("Starting DHCP listener on IPv4...");
+                    DHCPv4 listenerv4=new DHCPv4();
+                    listenerv4.start();
+                } catch (IOException e) {
+                    System.err.println("Failed to start IPv4 listener: "+e.getLocalizedMessage());
+                    if (strict) { System.exit(1); }
+                }
+            }
+            //-----
+            if (startipv6) {
+                try {
+                    System.out.println("Starting DHCP listener on IPv6...");
+                    DHCPv6 listenerv6=new DHCPv6();
+                    listenerv6.start();
+                } catch (IOException e) {
+                    System.err.println("Failed to start IPv4 listener: "+e.getLocalizedMessage());
+                    if (strict) { System.exit(1); }
+                }
+            }
+            //-----
+            System.out.println("Startup is complete, ready to service requests.");
         }
+        
+    private static void usage() {
+        System.out.println("Usage: java net.coagulate.JNKPU.NetworkUnlock [--noipv4] [--noipv6] [--strict] <keyfile>");
+        System.out.println("  --noipv4 - disable the IPv4 DHCP listener/responder (default is to start the IPv4 listener).");
+        System.out.println("  --noipv6 - disable the IPv6 DISH listener/responder (default is to start the IPv6 listener).");
+        System.out.println("  --strict - if any listener fails to start, exit (default is not to exit, so IPv6 may fail and IPv4 will still run).");
+        System.out.println("  keyfile  - path to Network Unlock private key in PKCS8 format with no password.");
+    }
+
+    private static void parseArguments(String[] args) {
+        if (args.length<1) { return; }
+        keyfile=args[args.length-1];
+        for (int i=0;i<(args.length-1);i++) {
+            boolean caught=false;
+            String arg=args[i];
+            if (arg.equalsIgnoreCase("--noipv4")) { startipv4=false; caught=true; }
+            if (arg.equalsIgnoreCase("--noipv6")) { startipv6=false; caught=true; }
+            if (arg.equalsIgnoreCase("--strict")) { strict=true; caught=true; }
+            if (!caught) { System.err.println("Unknown parameter '"+arg+"'"); usage(); System.exit(1); }
+        }
+    }
 }
